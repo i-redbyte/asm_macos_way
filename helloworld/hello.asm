038834ded8303dc37181bd5914bbe0ea17bdcd6b
Ilya Sokolov <developer.sokolov@gmail.com>
2024-08-07 15:15:15 +0300

    fix hello world

--- helloworld/hello.asm
+++ helloworld/hello.asm
@@ -1,14 +1,31 @@
 section .data
-    msg db  "Hello, world",0
+    align 16
+    msg db "Hello, world!", 10, 0
+    msg_len equ $ - msg
+
+    msg2 db "C the best language!", 10, 0
+    msg_len2 equ $ - msg2
 section .bss
+
 section .text
-    global _main
-_main:
-    mov rax,1
-    mov rdi,1
-    mov rsi,msg
-    mov rdx, 12
+    align 16
+    global _start
+_start:
+    ; Write syscall
+    mov rax, 0x02000004  ; syscall: write
+    mov rdi, 1           ; file descriptor: stdout
+    mov rsi, msg
+    mov rdx, msg_len
     syscall
-    mov rax, 60
-    mov rdi, 0
+
+    ; Write syscall
+    mov rax, 0x02000004  ; syscall: write
+    mov rdi, 1           ; file descriptor: stdout
+    mov rsi, msg2
+    mov rdx, msg_len2
+    syscall
+
+    ; Exit syscall
+    mov rax, 0x02000001  ; syscall: exit
+    xor rdi, rdi         ; exit code: 0
     syscall
